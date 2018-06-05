a7e28c875fb65a053710cf90f26af24587cbc63a
Kamal Kumar <kamalkumar1027@yahoo.com>
2018-06-05 11:58:56 -0400

    added sass file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 JW will update once completed
+
+triumphs:
+component, and getting a structured architecutre
+able to break down the psd in my head into what needs to go where
+
+defeats:
+getting images to render properly
+proper styling
+fonts
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './App.sass';
+
 import Nav from './components/nav';
 import Othernav from './components/othernav';
 import Main from './components/main';
